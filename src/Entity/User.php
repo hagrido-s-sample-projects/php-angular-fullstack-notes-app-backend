92831dd44fe343aa502cc6e59c86890f2308a3ce
hagrid862 <n.okroj@icloud.com>
2024-10-09 15:22:51 +0200

    feat: add User entity and migration for user and messenger_messages tables

---
+++ src/Entity/User.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ApiResource]
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    #[ORM\Column(length: 255, nullable: false, unique: true, type: 'string')]
+    private string $email;
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    #[ORM\Column(length: 255, nullable: false, unique: true, type: 'string')]
+    private string $username;
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    #[ORM\Column(length: 255, nullable: false, type: 'string')]
+    private string $password;
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+}
