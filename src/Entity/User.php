a45aaee1b41932c7b8950e433bbfc3e7ac62d9cc
hagrid862 <n.okroj@icloud.com>
2024-10-09 19:40:51 +0200

    feat: add created_at field to User entity and create migration for it feat: implement SessionRepository for session entity management

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -63,4 +63,19 @@ class User
         return $this;
     }
 
+    #[ORM\Column(type: 'datetime', nullable: false)]
+    private \DateTime $createdAt;
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+    
+
 }
