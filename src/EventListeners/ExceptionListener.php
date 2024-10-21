f0eec19d2b18807cbd64a23668d7d64ab5a93192
hagrid862 <n.okroj@icloud.com>
2024-10-21 08:24:54 +0200

    feat: added ExceptionListener to return all exceptions as json

---
+++ src/EventListeners/ExceptionListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $response = new JsonResponse();
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $response->setData([
+            'error' => [
+                'message' => $exception->getMessage(),
+                'code' => $exception->getCode()
+            ]
+        ]);
+
+        $event->setResponse($response);
+    }
+}
