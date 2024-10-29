dc43f4d2f60c2245e903fa21cb0495e637a16862
hagrid862 <n.okroj@icloud.com>
2024-10-29 08:11:58 +0100

    fix: updated name of get notes method

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -56,7 +56,7 @@ class NoteController extends AbstractController
         return new JsonResponse(['status' => 'SUCCESS', 'note' => $note->toArray()], Response::HTTP_CREATED);
     }
 
-    #[Route('s', name: 'app_note_get', methods: ['GET'])]
+    #[Route('s', name: 'app_notes_get', methods: ['GET'])]
     public function getNotes(Request $request): JsonResponse
     {
         $userId = $request->attributes->get('user_id');
